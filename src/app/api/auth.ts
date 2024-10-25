8951c6bd5046727af264c3f4002fb2f9fcfc45dc
Brayan <briang0325@gmail.com>
2024-10-24 21:26:48 -0500

    ADD url frontend deploy auth

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:4001/api/v1';
+const BASE_URL = 'https://image-plus-8621559e1d38.herokuapp.com/api/v1';
 
 export const singUpFetch = async (data: object) => {
     const response = await fetch(`${BASE_URL}/user/register`, {
